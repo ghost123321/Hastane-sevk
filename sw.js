2de0d621b8198c0a8b36ff3c7638289ecd7298bf
ghost123321 <47477279+ghost123321@users.noreply.github.com>
2025-10-18 16:52:26 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'hastene-cache-v1';
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/2965/2965567.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+    ))
+  );
+});
